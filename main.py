0532c3b2941fb75684277421a3afebd20a409bf9
NoNameAA <bbmmddkk@gmail.com>
2018-11-14 15:12:22 -0800

    finished input function and create df for two kinds of files

--- main.py
+++ main.py
@@ -4,17 +4,37 @@ import glob
 
 
 def input_data():
+
 	path = r'./fake-real-news/train/fake/'
 	txt_files = glob.glob(path + "*.txt")
-	print(txt_files)
+	
+	txt_df = pd.DataFrame(columns = ['word', 'truth'])	
 
 	for file in txt_files:
-		data = pd.read_csv(file, sep = " ", header = None)
-		data.columns = ["text", "truth"]
-		print(data)
-	# l = [pd.read_csv(file_name) for file_name in glob.glob(path + "/*.txt")]
-	# df = pd.concat(l, axis = 0)
-	# print(df)
+		with open(file, 'r') as f:
+			temp_list = []
+			for line in f:
+				for word in line.split():
+					temp_list.append(word)
+		txt_df = txt_df.append(pd.DataFrame({'word': [temp_list], 'truth': 'Fake'}))
+
+
+
+	path = r'./fake-real-news/train/real/'
+	txt_files = glob.glob(path + "*.txt")
+
+	for file in txt_files:
+		with open(file, 'r') as f:
+			temp_list = []
+			for line in f:
+				for word in line.split():
+					temp_list.append(word)
+		txt_df = txt_df.append(pd.DataFrame({'word': [temp_list], 'truth': 'Real'}))
+
+	txt_df = txt_df.reset_index(drop=True)
+	
+	print(txt_df)			
+	
 
 
 if __name__  ==  "__main__":
